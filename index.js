9254f9b9cae4e3f597a830a6ef26cf42eb70c9e5
liu-purnomo <kontak.liu@gmail.com>
2024-07-14 17:39:29 +0700

    feat: adding new package data

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static('./client/assets'));
 
 app.get('/', (req, res) => {
   res.sendFile('./client/index.html', {
